a5ae8bde062e7cc181a7d550b5e57ce67a7f6f73
trzymamryzwszafce <szymonmadeja523@gmail.com>
2026-08-08 19:21:26 +0200

    ą

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,5 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name) : name(name) {}
+
+Zombie::~Zombie() {}
---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,23 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+	private:
+		std::string name;
+
+	public:
+		Zombie();
+		~Zombie();
+
+		void	announce();
+};
+
+Zombie*	newZombie(std::string name);
+void	randomChump(std::string name);
+
+
+#endif
