955c37b8235d69c47e5f71b9fa7567319f9bfb05
trzymamryzwszafce <szymonmadeja523@gmail.com>
2026-08-21 21:29:20 +0200

    sdf

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,27 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+	private:
+		std::string		name;
+		unsigned int	hitPoints;
+		unsigned int	energyPoints;
+		unsigned int	attackDamage;
+
+	public:
+		ClapTrap(const std::string& clapName);
+		ClapTrap(const ClapTrap& obj);
+		ClapTrap& operator=(const ClapTrap& obj);
+		~ClapTrap();
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+};
+
+#endif
