3c3576f2b7059448f72f0c5295af1968dbb36a6c
trzymamryzwszafce <szymonmadeja523@gmail.com>
2026-08-28 20:39:13 +0200

     ą

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -4,50 +4,25 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
-int	main()
+int main()
 {
-	std::cout << "\n--- GOOD ANIMALS ---\n\n";
+	std::cout << "\n--- GIVEN MAIN ---\n\n";
 	{
-		const Animal* meta = new Animal();
-		const Animal* j = new Dog();
-		const Animal* i = new Cat();
-
-		std::cout << j->getType() << " " << std::endl;
-		std::cout << i->getType() << " " << std::endl;
-		i->makeSound(); //will output the cat sound!
-		j->makeSound();
-		meta->makeSound();
-
-		delete meta;
-		delete j;
-		delete i;
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	
+	delete j;
+	delete i;
 	}
-	std::cout << "\n--- WRONG ANIMALS ---\n\n";
+	std::cout << "\n--- 100 ANIMALS ---\n\n";
 	{
-		const WrongAnimal *meta = new WrongAnimal();
-		const WrongAnimal *cat = new WrongCat();
-
-		std::cout << meta->getType() << " " << std::endl;
-		std::cout << cat->getType() << " " << std::endl;
-		meta->makeSound();
-		cat->makeSound();
-
-		delete meta;
-		delete cat;
-	}
-	std::cout << "\n--- STACK ALLOCATION --- \n\n";
-	{
-		Animal animal;
-		Cat cat;
-		Dog dog;
-
-		std::cout << animal.getType() << " " << std::endl;
-		std::cout << cat.getType() << " " << std::endl;
-		std::cout << dog.getType() << " " << std::endl;
-
-		animal.makeSound();
-		cat.makeSound();
-		dog.makeSound();
+		const Animal *animals[10];
+		for (int i = 0; i < 5; i++)
+			animals[i] = new Dog();
+		for (int i = 5; i <= 10; i++)
+			animals[i] = new Cat();
+		for (int i = 0; i <= 10; i++)
+			delete animals[i];
 	}
 	return 0;
 }
